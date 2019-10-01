140372f731f5579c3f4a5dca45faaa045fa82047
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-01 15:15:04 -0500

    ejemplo rules
    script de uso de rules y su alternativa normal

---
+++ postgre_prog_en_servidor/rules/1_Regla_select.sql
@@ -0,0 +1,62 @@
+﻿-- Setup caso
+CREATE TABLE shoelace_data (
+    sl_name    text,          -- primary key
+    sl_avail   integer,       -- available number of pairs
+    sl_color   text,          -- shoelace color
+    sl_len     real,          -- shoelace length
+    sl_unit    text           -- length unit
+);
+
+INSERT INTO shoelace_data VALUES ('sl1', 5, 'negro', 80.0, 'cm');
+INSERT INTO shoelace_data VALUES ('sl2', 6, 'negro', 100.0, 'cm');
+INSERT INTO shoelace_data VALUES ('sl3', 0, 'negro', 35.0 , 'pulgada');
+INSERT INTO shoelace_data VALUES ('sl4', 8, 'negro', 40.0 , 'pulgada');
+INSERT INTO shoelace_data VALUES ('sl5', 4, 'cafe', 1.0 , 'm');
+INSERT INTO shoelace_data VALUES ('sl6', 0, 'cafe', 0.9 , 'm');
+INSERT INTO shoelace_data VALUES ('sl7', 7, 'cafe', 60 , 'cm');
+INSERT INTO shoelace_data VALUES ('sl8', 1, 'cafe', 40 , 'pulgada');
+
+CREATE TABLE unit (
+    un_name    text,          -- primary key
+    un_fact    real           -- factor to transform to cm
+);
+
+INSERT INTO unit VALUES ('cm', 1.0);
+INSERT INTO unit VALUES ('m', 100.0);
+INSERT INTO unit VALUES ('pulgada', 2.54);
+
+-- Create 3ra tabla para almacenar shoelace
+CREATE TABLE shoelace (
+    sl_name    text,          -- primary key
+    sl_avail   integer,       -- available number of pairs
+    sl_color   text,          -- shoelace color
+    sl_len     real,          -- shoelace length
+    sl_unit    text,           -- length unit
+    sl_len_cm    real          -- calculated len   
+    ) ;
+
+-- Crear regla
+CREATE RULE "_RETURN" AS ON SELECT TO shoelace DO INSTEAD
+    SELECT s.sl_name,
+           s.sl_avail,
+           s.sl_color,
+           s.sl_len,
+           s.sl_unit,
+           s.sl_len * u.un_fact AS sl_len_cm
+      FROM shoelace_data s, unit u
+     WHERE s.sl_unit = u.un_name;
+
+-- Seleccionar datos
+SELECT * 
+FROM shoelace;
+
+-- Limpiar
+
+-- Error (se considera como view)
+DROP TABLE shoelace;
+
+-- Funciona con el siguiente flujo 
+DROP VIEW shoelace;
+
+DROP TABLE shoelace_data;
+DROP TABLE unit;
---
+++ postgre_prog_en_servidor/rules/3_Regla_update.sql
@@ -0,0 +1,53 @@
+﻿-- Setup caso
+CREATE TABLE shoelace_data (
+    sl_name    text,          -- primary key
+    sl_avail   integer,       -- available number of pairs
+    sl_color   text,          -- shoelace color
+    sl_len     real,          -- shoelace length
+    sl_unit    text           -- length unit
+);
+
+INSERT INTO shoelace_data VALUES ('sl1', 5, 'negro', 80.0, 'cm');
+INSERT INTO shoelace_data VALUES ('sl2', 6, 'negro', 100.0, 'cm');
+INSERT INTO shoelace_data VALUES ('sl3', 0, 'negro', 35.0 , 'pulgada');
+INSERT INTO shoelace_data VALUES ('sl4', 8, 'negro', 40.0 , 'pulgada');
+INSERT INTO shoelace_data VALUES ('sl5', 4, 'cafe', 1.0 , 'm');
+INSERT INTO shoelace_data VALUES ('sl6', 0, 'cafe', 0.9 , 'm');
+INSERT INTO shoelace_data VALUES ('sl7', 7, 'cafe', 60 , 'cm');
+INSERT INTO shoelace_data VALUES ('sl8', 1, 'cafe', 40 , 'pulgada');
+
+CREATE TABLE shoelace_log (
+    sl_name    text,          -- shoelace changed
+    sl_avail   integer,       -- new available value
+    log_who    text,          -- who did it
+    log_when   timestamp      -- when
+);
+
+SELECT *
+FROM shoelace_data;
+
+SELECT *
+FROM shoelace_log;
+
+-- Crear regla
+CREATE RULE log_shoelace AS ON UPDATE TO shoelace_data
+    WHERE NEW.sl_avail <> OLD.sl_avail
+    DO INSERT INTO shoelace_log VALUES (
+                                    NEW.sl_name,
+                                    NEW.sl_avail,
+                                    current_user,
+                                    current_timestamp
+                                );
+
+-- Update datos
+UPDATE shoelace_data 
+SET sl_avail = 6 
+WHERE sl_name = 'sl7';
+
+-- Check tabla de bitacora (auditoria)
+SELECT * 
+FROM shoelace_log;
+
+-- Limpiar
+DROP TABLE shoelace_data;
+DROP TABLE shoelace_log;
